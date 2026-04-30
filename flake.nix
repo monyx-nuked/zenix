4fbb0c44fc39783fba21bbefda01ab70ffb265fd
monyxoid <helix.nuked@proton.me>
2026-04-30 14:04:00 +0500

    chore(flake): add description, inputs, lockfile
    -> Add description of the flake
    -> Add necessary inputs ( nixpkgs obviously )
    -> Create a lockfile

--- flake.nix
+++ flake.nix
@@ -1,3 +1,13 @@
 {
+  description = "flake for zenix project/configuration (monyxoid/zenix)";
+  inputs = {
+    nixpkgs = {
+      # nixpkgs
+      # > use nixos-unstable for stability
+      # > largest repository of the linux world
+      # > every packages has binary
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+  };
   outputs = { self }: { };
 }
