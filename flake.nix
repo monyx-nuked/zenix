61da96c4feab34f4728d51ae582a47f5e36599b0
monyxoid <helix.nuked@proton.me>
2026-04-30 14:14:01 +0500

    feat(license)!: add license for this project and install framework
    -> License: The Unlicense
    
    -> Installed framework: flake-parts
    ->> Plugins: import-tree (Automatically imports every *.nix file from
    specified directory )
    --> Made a whoopsie when creating a commit and amended it apparently
    
    BREAKING CHANGE: If you use this project then
    		 I advice you to read the license
    		 Or altarnatively get summary of it

--- flake.nix
+++ flake.nix
@@ -1,13 +1,26 @@
 {
   description = "flake for zenix project/configuration (monyxoid/zenix)";
   inputs = {
+    # essentials #
     nixpkgs = {
       # nixpkgs
-      # > use nixos-unstable for stability
-      # > largest repository of the linux world
-      # > every packages has binary
+      # > using nixos-unstable for latest packages
+      # > is largest repository of the linux world
+      # > it has binary cache for every package...
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
+    # framework #
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+    };
+    # libraries #
+    nixpkgs-lib = {
+      url = "github:nix-community/nixpkgs.lib";
+    };
+    import-tree = {
+      url = "github:denful/import-tree";
+    };
   };
-  outputs = { self }: { };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
