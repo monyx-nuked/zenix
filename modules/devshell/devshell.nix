c3aef94569907f5ff12eb9cc76238faa046f47fe
monyxoid <helix.nuked@proton.me>
2026-05-01 21:40:58 +0500

    feat(devshell): add aliases, tools, change motd
    > Changes:
    >> Add utilities and alias them to common commands
     - bat => cat
     - eza => ls
     - convco commit => convcommit
    >> Make MOTD spaced, fix grammar, make look good on mono font

--- modules/devshell/devshell.nix
+++ modules/devshell/devshell.nix
@@ -7,15 +7,38 @@
     { pkgs, ... }:
     {
       devshells.default = {
+        commands = [
+          {
+            help = "alias for bat";
+            name = "cat";
+            command = "bat $@";
+          }
+          {
+            help = "alias for ls";
+            name = "ls";
+            command = "eza $@";
+          }
+          {
+            help = "alias for convco commit";
+            name = "convcommit";
+            command = "convco commit $@";
+          }
+        ];
         devshell = {
           name = "default-devshell";
           motd = ''
-            {202}>= Welcome to devshell!{reset}
-            {203}This was made with numtide/devshells
-            This thing was made for this project{reset}
+            {202}
+            >= Welcome to devshell!
+            {reset}{203}
+            This is made with numtide/devshells
+            This thing is made for this project
+            {reset}
           '';
           packages = with pkgs; [
             nh
+            bat
+            eza
+            convco
           ];
         };
       };
